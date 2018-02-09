222d087bd992975af1d4e72609cc8fbfec93cc9f
zhenbing <zjh@cnic.cn>
2018-02-09 15:59:35 +0800

    if the polygon is larger than the raster

--- maskimage.py
+++ maskimage.py
@@ -147,17 +147,33 @@ def mask_image_by_geometry(geomjson, geomproj, raster, tag, name):
     ll = raster.index(*geometry.bounds[0:2])  # lowerleft bounds[0:2] xmin, ymin
     ur = raster.index(*geometry.bounds[2:4])  # upperright bounds[2:4] xmax, ymax
 
+    # # create an affine transform for the subset data
+    # t = raster.transform
+    # shifted_affine = Affine(t.a, t.b, t.c + ll[1] * t.a, t.d, t.e, t.f + ur[0] * t.e)
+    #
+    # # read the subset of the data into a numpy array
+    # window = ((ur[0], ll[0] + 1), (ll[1], ur[1] + 1))
+
+    # when the shapefile polygon is larger than the raster
+    row_begin = ur[0] if ur[0] > 0 else 0
+    row_end = ll[0] + 1 if ll[0] > -1 else 0
+    col_begin = ll[1] if ll[1] > 0 else 0
+    col_end = ur[1] + 1 if ur[1] > -1 else 0
+    window = ((row_begin, row_end),(col_begin, col_end))
+
     # create an affine transform for the subset data
     t = raster.transform
-    shifted_affine = Affine(t.a, t.b, t.c + ll[1] * t.a, t.d, t.e, t.f + ur[0] * t.e)
-
-    # read the subset of the data into a numpy array
-    window = ((ur[0], ll[0] + 1), (ll[1], ur[1] + 1))
+    shifted_affine = Affine(t.a, t.b, t.c + col_begin * t.a, t.d, t.e, t.f + row_begin * t.e)
 
     out_data = raster.read(window=window)
-    if len(out_data)==0:
+
+    # check whether the numpy array is empty or not?
+    if out_data.size == 0 or np.all(out_data == raster.nodata):
         print('the grid does not intersect with the raster')
         return
+    # if len(out_data)==0:
+    #     print('the grid does not intersect with the raster')
+    #     return
     with rasterio.open("/tmp/mask_%s.tif" % name, 'w', driver='GTiff', width=out_data.shape[2], height=out_data.shape[1], crs=raster.crs,
                        transform=shifted_affine, dtype=np.uint16, nodata=256, count=raster.count,
                        indexes=raster.indexes) as dst:
