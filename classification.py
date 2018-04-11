a2c0069a1b722f1e8531de5865bf73074e5e5433
zhenbing <zjh@cnic.cn>
2018-04-11 11:17:33 +0800

    svm model input data has Nodata

--- classification.py
+++ classification.py
@@ -1,6 +1,6 @@
 import rasterio
 import os, re
-import matplotlib.pyplot as plt
+
 import numpy as np
 from affine import Affine
 import rasterio.features
@@ -24,11 +24,11 @@ class Remote_Classification:
 
         imagepixel_trainingdata = Imagepixel_Trainingdata(self.file_root, self.image_bands_dir, self.sensor)
         training_X, training_y = imagepixel_trainingdata.get_training_data()
-        self.train_x, self.train_y, self.test_x, self.test_y = imagepixel_trainingdata.split_dataset(training_X, training_y, 0.1)
+        self.train_x, self.train_y, self.test_x, self.test_y = imagepixel_trainingdata.split_dataset(training_X,
+                                                                                                     training_y, 0.1)
         print('reading image data %s to be processed....' % imagepixel_trainingdata.image_bands_dir.split('/')[-1])
         X, self.src_profile = imagepixel_trainingdata.read_bands_to_be_classified()
         self.X = imagepixel_trainingdata.gen_features(X)
-
 
     def SVM(self):
         model_pkl = os.path.join(self.file_root, '/models/svm.pkl')
@@ -44,6 +44,9 @@ class Remote_Classification:
         print('using SVM model to predict the image.....')
         row = self.src_profile['height']
         col = self.src_profile['width']
+
+        # 由于影像边缘存在NoData，因此需对边缘NoData位置或者index作标记，预测的结果也直接写入NoData,所以需要提前建好预测结果，并将NoData提案其中，剩下的位置则由预测模型的结果填入。
+        # 不能直接使用self.X
         img_SVM = clf.predict(self.X).reshape(row, col)
 
         dst_path = os.path.join(self.file_root, '/results/%s.tif' % self.image_bands_dir.split('/')[-1])
@@ -61,8 +64,8 @@ class Remote_Classification:
             with rasterio.open(dst_path, 'w', **profile) as dst:
                 dst.write(img_SVM.astype(rasterio.uint8), 1)
 
-        # save the model by using pickle
-        # joblib.dump(clf, model_pkl)
+                # save the model by using pickle
+                # joblib.dump(clf, model_pkl)
 
     def RandomForest(self):
         model_npy = os.path.join(self.file_root + '/models/Z_random_forest.npy')
@@ -102,58 +105,12 @@ class Remote_Classification:
             np.save(model_npy, Z_KMeans)
         return Z_KMeans
 
-    def plot_results(self):
-        plt.figure(figsize=(10, 7))
-
-        ax = plt.subplot(231)
-        ax.set_title("Original")
-        plt.imshow(self.img)
-
-        ax = plt.subplot(232)
-        ax.set_title("Reference Map")
-        plt.imshow(self.label)
-
-        ax = plt.subplot(233)
-        ax.set_title("SVM Result")
-        Z_svm = self.SVM()
-        plt.imshow(Z_svm)
-
-        ax = plt.subplot(234)
-        ax.set_title("Random Forest Result")
-        Z_random_forest = self.RandomForest()
-        plt.imshow(Z_random_forest)
-
-        ax = plt.subplot(235)
-        ax.set_title("KNN Result (k=10)")
-        Z_knn = self.KNN(10)
-        plt.imshow(Z_knn)
-
-        ax = plt.subplot(236)
-        ax.set_title("KNN Result (k=50)")
-        Z_knn = self.KNN(50)
-        plt.imshow(Z_knn)
-        plt.show()
-
-    def stretch_plot(self):
-        ax = plt.subplot(221)
-        img = self.img
-        plt.imshow(img)
-        ax = plt.subplot(222)
-        plt.hist(img.reshape(512 * 512, 3), color=['red', 'green', 'blue'])
-        ax = plt.subplot(223)
-        img_ = self.stretch(img)
-        plt.imshow(img_)
-        ax = plt.subplot(224)
-        plt.hist(img_.reshape(512 * 512, 3), color=['red', 'green', 'blue'])
-        plt.show()
-
 
 def test_classification():
     file_root = '/mnt/win/water_paper/training_data/TM'
     images_to_be_classified = os.path.join(file_root, 'image/L5134036')
     remote_class = Remote_Classification(file_root, images_to_be_classified, 'TM')
     remote_class.SVM()
-
 
 
 if __name__ == '__main__':
